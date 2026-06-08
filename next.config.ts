f7ebb1b342a3377d3bfdfb16ab8a44f483a70c94
NoeRelic (Batın Savaş) <batinsavas.dev@gmail.com>
2026-06-08 14:37:23 +0300

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   // Increase max body size for large M3U playlist chunks sent from admin panel
-  middlewareClientMaxBodySize: 50 * 1024 * 1024, // 50MB
+ // middlewareClientMaxBodySize: 50 * 1024 * 1024, // 50MB
 };
 
 export default nextConfig;
